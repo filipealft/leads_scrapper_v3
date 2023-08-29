ecf5b049dde870bdfba39c058440fe6281040db6
Filipe Alano Fortunato <62904031+filipealft@users.noreply.github.com>
2023-08-28 22:09:23 -0300

    a

--- message_bot/wpp_message.py
+++ message_bot/wpp_message.py
@@ -1,68 +1,48 @@
-import subprocess
-import psutil
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class WhatsAppBot():
-
     def __init__(self):
         self.driver = None
 
-    def start_chrome_debugger(self):
-        for proc in psutil.process_iter(attrs=['pid', 'name', 'cmdline']):
-            if 'Google Chrome' in proc.info['name'] and '--remote-debugging-port=9222' in proc.info['cmdline']:
-                time.sleep(1)
-                print("Chrome com depurador remoto já está em execução.")
-                return
-        try:
-            subprocess.Popen([
-                "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome", # Caminho para o Chrome no Mac
-                "--remote-debugging-port=9222"
-            ])
-            time.sleep(5) 
-            print("Chrome com depurador remoto iniciado.")
-        except FileNotFoundError:
-            print("Caminho do Chrome incorreto ou o Chrome não está instalado.")
-        except Exception as e:
-            print(f"Erro ao iniciar o Chrome: {e}")
-
     def init_instance_chrome(self):
         chrome_options = Options()
-        chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
         chrome_options.add_argument("--ignore-certificate-errors")
+        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox") 
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("user-data-dir=selenium")
 
+        self.driver = webdriver.Chrome(options=chrome_options)
 
-        if not self.driver:
-            self.driver = webdriver.Chrome(options=chrome_options)
+    def send_messages(self, phone_numbers, message):
 
-        return self.driver
+        for phone in phone_numbers:
+            self.driver.get(f'https://web.whatsapp.com/send?phone={phone}')
+            time.sleep(10)
+            text_box = WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.XPATH, '//div[@contenteditable="true" and @title="Mensagem"]')))
 
-    def send_message(self, phone_number, message):
-        if not self.driver:
-            print("Driver not initialized. Call init_instance_chrome() first.")
-            return
-
-        # self.driver.get('https://web.whatsapp.com/')
-        # input("Pressione Enter depois de escanear o QR code...")
-        self.driver.get(f'https://web.whatsapp.com/send?phone={phone_number}')
-        time.sleep(5)
-        text_box = self.driver.find_element(By.XPATH, '//div[@contenteditable="true" and @title="Mensagem"]')
-        text_box.send_keys(message) 
-        time.sleep(3) 
-        text_box.send_keys(Keys.ENTER)
+            text_box.send_keys(message)
+            time.sleep(3)
+            text_box.send_keys(Keys.ENTER)
+            time.sleep(3) 
 
     def quit(self):
         if self.driver:
             self.driver.quit()
 
 bot = WhatsAppBot()
-bot.start_chrome_debugger()  
-bot.init_instance_chrome()   
 
-phone_number = '4899554102' 
+phone_numbers = ['4899554102', '4884948169'] 
 message = 'Olá! Esta é uma mensagem automática!'
-bot.send_message(phone_number, message)
 
+while True:
+    bot.init_instance_chrome()
+    bot.send_messages(phone_numbers, message)
+    bot.quit() 
+    time.sleep(30)  
